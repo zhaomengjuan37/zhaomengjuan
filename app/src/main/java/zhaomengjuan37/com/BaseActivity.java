b6e1e2e96a80eed98ca363562b1fc955a9f32caf
zhaomengjuan37 <3329368917@qq.com>
2020-06-28 11:27:02 +0800

    first commit

---
+++ app/src/main/java/zhaomengjuan37/com/BaseActivity.java
@@ -0,0 +1,11 @@
+package zhaomengjuan37.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
